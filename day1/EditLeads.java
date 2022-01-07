aeadccc9503f2e28db9eb8135bd7b9177339f515
bhaavyasid <72611177+bhaavyasid@users.noreply.github.com>
2022-01-07 17:58:25 +0530

    Add files via upload

--- day1/EditLeads.java
+++ day1/EditLeads.java
@@ -8,20 +8,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class EditLeads {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver= new ChromeDriver();
+		ChromeDriver driver=new ChromeDriver();
 		driver.get("http://leaftaps.com/opentaps/control/login");
 		driver.manage().window().maximize();
-	   WebElement username = driver.findElement(By.id("username"));
-		username.sendKeys("DemoSalesManager");
-           driver.findElement(By.id("password")).sendKeys("crmsfa");
-           driver.findElement(By.className("decorativeSubmit")).click();
-           driver.findElement(By.linkText("CRM/SFA")).click();
-           driver.findElement(By.linkText("Leads")).click();
-           driver.findElement(By.linkText("Find Leads")).click();
-           
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("DemoSalesManager");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		driver.findElement(By.xpath("//a[text()='Contacts']")).click();
+	    Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[text()='Find Contacts']")).click();
+		String str1="10004";
+        driver.findElement(By.xpath("//input[@name='id']")).sendKeys(str1);
+        driver.findElement(By.xpath("//button[text()='Find Contacts']")).click();
+        Thread.sleep(3000);
+        //driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a[@class='linktext']")).click();
+        Thread.sleep(3000);        
+        driver.findElement(By.xpath("//a[text()='Edit']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@id='updateContactForm_firstName']")).clear();
+        driver.findElement(By.xpath("//input[@id='updateContactForm_firstName']")).sendKeys("siva");
+
+        driver.findElement(By.xpath("//input[@value='Update']")).click();
 	}
 
 }
+	
